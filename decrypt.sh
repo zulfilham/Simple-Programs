0e04cdba0f9c659a65e1d3bcc54934da3b90a07c
Zulfilham <code.zulfilham@gmail.com>
2021-10-06 00:42:36 +0700

    Fixes order of decryption

--- decrypt.sh
+++ decrypt.sh
@@ -15,37 +15,39 @@ function read_encryption_key() {
 function main() {
    read_encryption_key "Enter decryption key: ";
    echo "*** Decrypting file names ***";
-   local cpt_suffixed_filenames=() base64_encrypted_basename decrypted_basename exit_code digest dirname basename;
+   local cpt_suffixed_filenames=() path base64_encrypted_basename decrypted_basename exit_code digest dirname basename;
 
    while read -d $'\0'; do
       cpt_suffixed_filenames+=("$REPLY");
    done < <(find "$@" -mindepth 1 -depth \( -name \*.cpt -and \! -name .cpt -or -type d \) -print0);
 
-   while read -d $'\0'; do
-      for base64_encrypted_basename in $(cat "$REPLY"); do
-         decrypted_basename="$(base64 --decode <<< $base64_encrypted_basename | ccat --envvar=ENCRYPTION_KEY)";
-         exit_code=$?;
+   for path in "${@-.}"; do
+      while read -d $'\0'; do
+         for base64_encrypted_basename in $(cat "$REPLY"); do
+            decrypted_basename="$(base64 --decode <<< $base64_encrypted_basename | ccat --envvar=ENCRYPTION_KEY)";
+            exit_code=$?;
 
-         if ((exit_code == 0)); then
-            digest=($(xxh128sum <<< $base64_encrypted_basename));
+            if ((exit_code == 0)); then
+               digest=($(xxh128sum <<< $base64_encrypted_basename));
 
-            for i in "${!cpt_suffixed_filenames[@]}"; do
-               dirname="${cpt_suffixed_filenames[i]%/*}"; [ "${cpt_suffixed_filenames[i]}" == "$dirname" ] && dirname=.;
-               basename="${cpt_suffixed_filenames[i]##*/}";
+               for i in "${!cpt_suffixed_filenames[@]}"; do
+                  dirname="${cpt_suffixed_filenames[i]%/*}"; [ "${cpt_suffixed_filenames[i]}" == "$dirname" ] && dirname=.;
+                  basename="${cpt_suffixed_filenames[i]##*/}";
 
-               if [ "${basename%.cpt}" == ${digest[0]} ]; then
-                  mv --verbose -- "$dirname/"${digest[0]}* "$dirname/${decrypted_basename%$'\n'}";
-                  sed --in-place "/${base64_encrypted_basename//\//\\\/}/d" -- "$dirname/.cpt";
-                  unset cpt_suffixed_filenames[i];
-                  break;
-               fi;
-            done;
-         else
-            return $exit_code;
-         fi;
-      done;
-      if [ ! -s "$REPLY" ]; then rm --force -- "$REPLY"; fi;
-   done < <(find "$@" -mindepth 1 -name .cpt -print0 | tac --separator=$'\0');
+                  if [ "${basename%.cpt}" == ${digest[0]} ]; then
+                     mv --verbose -- "$dirname/"${digest[0]}* "$dirname/${decrypted_basename%$'\n'}";
+                     sed --in-place "/${base64_encrypted_basename//\//\\\/}/d" -- "$dirname/.cpt";
+                     unset cpt_suffixed_filenames[i];
+                     break;
+                  fi;
+               done;
+            else
+               return $exit_code;
+            fi;
+         done;
+         if [ ! -s "$REPLY" ]; then rm --force -- "$REPLY"; fi;
+      done < <(find "$path" -mindepth 1 -name .cpt -print0 | sort --zero --reverse);
+   done;
    echo $'\n*** Decrypting file contents ***';
    echo -n "$ENCRYPTION_KEY" | ccdecrypt --verbose --force --recursive --keyfile=- -- "${@%/}";
 }
